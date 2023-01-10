893c8f1c8bbbfc1f533db16cea410ddd44346ece
CD <95479866+Warrior2852@users.noreply.github.com>
2023-01-10 10:36:18 +0000

    File updates

---
+++ Day 05/05p2.py
@@ -0,0 +1,24 @@
+file = open('day5.txt', 'r')
+lines = file.readlines()
+ready = 0
+grid = [["N","C","R","T","M","Z","P"],["D","N","T","S","B","Z"],["M","H","Q","R","F","C","T","G"],["G","R","Z"],["Z","N","R","H"],["F","H","S","W","P","Z","L","D"],["W","D","Z","R","C","G","M"],["S","J","F","L","H","W","Z","Q"],["S","Q","P","W","N"]]
+for line in lines:
+    if line == "\n":
+        ready = 1
+        continue
+    if ready == 0:
+        continue
+    line = line.strip()
+    instruct = line.split(" ")
+    num = int(instruct[1])
+    origin = (int(instruct[3]))-1
+    to = (int(instruct[5]))-1
+    buffer = []
+    for i in range(0, num):
+        buffer.append(grid[origin].pop())
+    buffer.reverse()
+    grid[to].extend(buffer)
+ans = ""
+for j in range(0, len(grid)):
+    ans = ans + grid[j][-1]
+print(ans)
---
+++ Day 06/06p1.py
@@ -0,0 +1,17 @@
+file = open('day6.txt', 'r')
+lines = file.readlines()
+count = 1
+last = []
+for line in lines:
+    line = line.strip()
+    chars = [char for char in line]
+    for i in range(0, len(chars)):
+        if len(last) == 4:
+            last.pop(0)
+        last.append(chars[i])
+        if len(last) == 4:
+            if len(set(last)) == len(last):
+                print(set(last))
+                break
+        count += 1
+print(count)
